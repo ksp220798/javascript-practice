89807b9aad21faf6c303e6482f4a342b4a79d8a4
ksp220798 <ksp220798@gmail.com>
2025-06-17 21:34:30 +0530

    type check

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,11 @@
 console.log("hello world.");
+
+//let languages = ["HTML", "CSS", "JavaScript"];
+let languages = {
+    name : "kundan"
+};
+
+console.log(languages instanceof Array);
+console.log(languages instanceof Object);
+console.log(languages instanceof String);
+console.log(languages instanceof Number);
